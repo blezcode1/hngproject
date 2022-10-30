b56c006a37cdb09b38fa8248e93ad7e8a86af4e3
blezcode1 <blessingocheje99@gmail.com>
2022-10-30 14:27:04 +0100

    hng task in progress

---
+++ my_component.js
@@ -0,0 +1,23 @@
+'use strict'; 
+const e = React.createElement; 
+class myButton extends React.Component { 
+  constructor(props) { 
+    super(props); 
+    this.state = { isliked: false }; 
+  } 
+ 
+  render() { 
+    if (this.state.isliked) { 
+      return ' Hi,I am blessing'; 
+    } 
+ 
+    return e( 
+      'button', 
+      { onClick: () => this.setState({ isliked: true }) }, 
+      'Clik Me' 
+    ); 
+  } 
+} 
+const domContainer = document.querySelector('#some_random_id'); 
+ReactDOM.render(e(myButton), domContainer);
+
